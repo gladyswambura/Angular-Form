1e8953e3f7f92bea5165f283bcba8166ce8e4341
gladyswambura <gladyswahito7@gmail.com>
2022-04-08 13:08:35 +0300

    Final commit

---
+++ src/app/formcomponent/formcomponent.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formcomponent',
+  templateUrl: './formcomponent.component.html',
+  styleUrls: ['./formcomponent.component.css']
+})
+export class FormcomponentComponent {
+  submit(login: any) {
+    console.log("form submitted",login)
+  }
+}
